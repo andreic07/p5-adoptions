b83f62f6f42f52d5dc8cb97909df33edc3c1851c
andreic07 <remhq98@gmail.com>
2021-05-12 19:38:44 +0300

    Initial commit

---
+++ src/main/java/com/p5/adoptions/AdoptionsApplication.java
@@ -0,0 +1,13 @@
+package com.p5.adoptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptionsApplication.class, args);
+	}
+
+}
